abb1844b221f9902e9f5cae316867431d1ba5cd7
jontoh <75199733+jontohh@users.noreply.github.com>
2024-09-30 13:55:22 +0800

    added FE template + timperio logo

---
+++ frontend/src/features/user/LandingIntro.js
@@ -0,0 +1,20 @@
+import TemplatePointers from "./components/TemplatePointers"
+
+
+
+function LandingIntro(){
+
+    return(
+        <div className="hero min-h-full rounded-l-xl bg-base-200">
+            <div className="hero-content py-12">
+              <div className="max-w-md">
+              <h1 className='text-3xl text-center font-bold '><img src="/Timperio_logo.png" className="w-15 inline-block mr-2 mask" alt="Timperio Logo" /></h1>          
+              </div>
+
+            </div>
+          </div>
+    )
+      
+  }
+  
+  export default LandingIntro
