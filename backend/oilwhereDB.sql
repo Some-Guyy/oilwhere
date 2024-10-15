6783c88fc344112b2b440a704a0b06b82b401190
Some Guy <somenormaladdress@gmail.com>
2024-10-15 18:10:20 +0800

    allow null for sale_type, digital, shipping_method, product in sql script

--- backend/oilwhereDB.sql
+++ backend/oilwhereDB.sql
@@ -7,12 +7,12 @@ DROP TABLE IF EXISTS `purchase_history`;
 CREATE TABLE IF NOT EXISTS `purchase_history` (
   `purchase_id` INT NOT NULL AUTO_INCREMENT,
   `sale_date` date NOT NULL,
-  `sale_type` varchar(255) NOT NULL,
-  `digital`  varchar(255) NOT NULL,
+  `sale_type` varchar(255),
+  `digital`  varchar(255),
   `customer_id` int NOT NULL,
   `zipcode` int(6),
-  `shipping_method` varchar(255) NOT NULL,
-  `product` varchar(255) NOT NULL,
+  `shipping_method` varchar(255),
+  `product` varchar(255),
   `variant` int NOT NULL,
   `quantity` int NOT NULL,
   `price` decimal(10,2) NOT NULL,
@@ -25,12 +25,12 @@ DROP TABLE IF EXISTS `purchase_history_temp`;
 CREATE TABLE IF NOT EXISTS `purchase_history_temp` (
   `purchase_id` INT NOT NULL AUTO_INCREMENT,
   `sale_date` VARCHAR(10) NOT NULL,  -- Store date as VARCHAR first
-  `sale_type` VARCHAR(255) NOT NULL,
-  `digital`  VARCHAR(255) NOT NULL,
+  `sale_type` VARCHAR(255),
+  `digital`  VARCHAR(255),
   `customer_id` INT NOT NULL,
   `zipcode` INT(6),
-  `shipping_method` VARCHAR(255) NOT NULL,
-  `product` VARCHAR(255) NOT NULL,
+  `shipping_method` VARCHAR(255),
+  `product` VARCHAR(255),
   `variant` INT NOT NULL,
   `quantity` INT NOT NULL,
   `price` DECIMAL(10,2) NOT NULL,
@@ -54,7 +54,7 @@ CREATE TABLE IF NOT EXISTS `newsletter` (
   `design_id` INT NOT NULL AUTO_INCREMENT,
   `content` text NOT NULL,
   PRIMARY KEY (`design_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULTs CHARSET=utf8;
 
 -- Loading csv data into purchase_history
 LOAD DATA INFILE 'C:/wamp64/tmp/Sales_Data_updated_Sep 2023.csv'
@@ -62,9 +62,13 @@ INTO TABLE `purchase_history_temp`
 FIELDS TERMINATED BY ',' 
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS
-(@discard, sale_date, sale_type, digital, customer_id, @vzipcode, shipping_method, product, variant, quantity, price, product_price)
+(@discard, sale_date, @vsale_type, @vdigital, customer_id, @vzipcode, @vshipping_method, @vproduct, variant, quantity, price, product_price)
 SET
+sale_type = NULLIF(@vsale_type, '')
+digital = NULLIF(@vdigital, '')
 zipcode = NULLIF(@vzipcode, '')
+shipping_method = NULLIF(@vshipping_method, '')
+product = NULLIF(@vproduct, '')
 ;
 
 -- Insert data into the final table with date conversion
