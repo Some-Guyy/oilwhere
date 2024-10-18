721263afe7eb9f4fdb2aa15f94f961943646e1d9
jontoh <75199733+jontohh@users.noreply.github.com>
2024-10-18 15:53:50 +0800

    be: package.sh: remove source condition

--- backend/package.sh
+++ backend/package.sh
@@ -1,9 +1,3 @@
-#!/bin/bash
-if [ "$BASH_SOURCE" = "" ] ; then
-    echo "This script should be sourced, not executed. Run \"source ./package.sh\" instead."
-    exit 1
-fi
-
 set -a
 source .env
 set +a
