f8620c0ce5dea8681a83e85019309e087b24869b
Ryan2409 <ryan.ng.2022@scis.smu.edu.sg>
2024-11-08 17:03:55 +0800

    finished comments for filter for customer, filter and purchase, service layers

--- backend/src/main/java/crm/oilwhere/service/FilterService.java
+++ backend/src/main/java/crm/oilwhere/service/FilterService.java
@@ -7,23 +7,32 @@ import org.springframework.stereotype.Service;
 import crm.oilwhere.model.Filter;
 import crm.oilwhere.repository.FilterRepository;
 
+// FilterService is the service layer connected to the FilterRepository. It contains all the business logic for customer_spending_ranked table. It utilises the JPA repository to query from the database.
+// A Filter record consist of customer id and their total spending
+// consists of the following functions
+// getAllCustomerSpending() -- retrieve all Filter records
+// getHighValueCustomer() -- retrieve customers whose total spending is at top 10% 
+// getMediumValueCustomer() --  retrieve customers whose total spending is between top 10% and bottom 20%
+// getLowValueCustomer() -- retrieve customers whose total spending is at bottom 20% 
 
 @Service
 public class FilterService {
 
     private final FilterRepository filterRepository;
 
-    // constructor
+    // constructor to create FilterService object
     public FilterService(FilterRepository filterRepository) {
         this.filterRepository = filterRepository;
     }
 
     // get all purchase from purchase history
+    // Uses JPA repository findAll function to retrieve all Filter records
     public List<Filter> getAllCustomerSpending() {
         return filterRepository.findAll();
     }
 
     // get high-value customer(top 10%)
+    // Uses custome query method from JPA repository, findAllByOrderByTotalSpendingDesc, to retrieve all customer records that are top 10% in spending
     public List<Filter> getHighValueCustomer() {
 
         // get list of all customers sorted by total spending descending
@@ -37,6 +46,7 @@ public class FilterService {
     }
     
     // get medium-value customer(between 10% and 80%)
+    // Uses custome query method from JPA repository, findAllByOrderByTotalSpendingDesc, to retrieve all customer records that are between top 10% and bottom 20% in spending
     public List<Filter> getMediumValueCustomer() {
 
         // get list of all customers sorted by total spending ascending
@@ -53,6 +63,7 @@ public class FilterService {
     }
 
     // get low-value customer(bottom 20%)
+    // Uses custome query method from JPA repository, findAllByOrderByTotalSpendingDesc, to retrieve all customer records that are bottom 20% in spending
     public List<Filter> getLowValueCustomer() {
 
         // get list of all customers sorted by total spending ascending
