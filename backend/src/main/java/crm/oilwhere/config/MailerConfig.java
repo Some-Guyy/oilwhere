9d195cd5e5df99b09776c932712f10fff5f4d2ae
Some Guy <somenormaladdress@gmail.com>
2024-11-08 18:32:42 +0800

    added comments for EmailBuilderUtil.java and MailerConfig.java

--- backend/src/main/java/crm/oilwhere/config/MailerConfig.java
+++ backend/src/main/java/crm/oilwhere/config/MailerConfig.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+// Config for building the Mailer object which handles the mail server and sending of emails
 @Configuration
 public class MailerConfig {
 
+    // These values come from resources/application.properties
     @Value("${mail.smtp.host}")
     private String smtpHost;
 
--- backend/src/main/java/crm/oilwhere/util/EmailBuilderUtil.java
+++ backend/src/main/java/crm/oilwhere/util/EmailBuilderUtil.java
@@ -6,7 +6,13 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class EmailBuilderUtil {
-
+    // Use simplejavamail's EmailBuilder class to construct an Email object
+    // fromName: Name of sender (sender will be us)
+    // fromAddr: Email address of sender
+    // toName: Name of recipient
+    // toAddr: Email address of recipient
+    // subject: Email subject
+    // htmlBody: Email body using html
     public Email buildHtmlEmail(String fromName, String fromAddr, String toName, String toAddr, String subject, String htmlBody) {
         return EmailBuilder.startingBlank()
                 .from(fromName, fromAddr)
@@ -15,6 +21,4 @@ public class EmailBuilderUtil {
                 .withHTMLText(htmlBody)
                 .buildEmail();
     }
-
-    // Additional methods for HTML emails, attachments, etc.
 }
