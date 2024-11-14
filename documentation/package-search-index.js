b5eb98e9e57c52a1f56503d318eeeedeaee11d6f
brandonlhk <brandon.lim.2022@scis.smu.edu.sg>
2024-11-14 18:27:33 +0800

    removed documentation folder

--- documentation/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"crm.oilwhere"},{"l":"crm.oilwhere.config"},{"l":"crm.oilwhere.controller"},{"l":"crm.oilwhere.dto"},{"l":"crm.oilwhere.model"},{"l":"crm.oilwhere.repository"},{"l":"crm.oilwhere.service"},{"l":"crm.oilwhere.util"}];updateSearchResults();
