a5898ed6087c02b44ecfd1df67017dbba38444a2
brandonlhk <brandon.lim.2022@scis.smu.edu.sg>
2024-11-22 20:43:01 +0800

    bug fix

--- backend/src/main/java/crm/oilwhere/model/MediumValueCustomerFilter.java
+++ backend/src/main/java/crm/oilwhere/model/MediumValueCustomerFilter.java
@@ -8,7 +8,7 @@ public class MediumValueCustomerFilter extends CustomerSegmentFilter {
     @Override
     public List<CustomerSpending> applyFilter(List<CustomerSpending> customers) {
         int bottomBoundary = (int) Math.ceil(customers.size() * 0.2);
-        int upperBoundary = (int) Math.ceil(customers.size() * 0.8);
+        int upperBoundary = (int) Math.ceil(customers.size() * 0.9);
         return customers.subList(bottomBoundary, upperBoundary);
     }
 }   
